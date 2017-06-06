01b4ca04d5c340556d3136aa54924f058c28c15a
ngc92 <ngc92@users.noreply.github.com>
2017-06-06 17:45:07 +0200

    gradient clipping into name scope

--- baselines/common/tf_util.py
+++ baselines/common/tf_util.py
@@ -205,9 +205,10 @@ def minimize_and_clip(optimizer, objective, var_list, clip_val=10):
     variable is clipped to `clip_val`
     """
     gradients = optimizer.compute_gradients(objective, var_list=var_list)
-    for i, (grad, var) in enumerate(gradients):
-        if grad is not None:
-            gradients[i] = (tf.clip_by_norm(grad, clip_val), var)
+    with tf.name_scope("gradient_clipping"):
+        for i, (grad, var) in enumerate(gradients):
+            if grad is not None:
+                gradients[i] = (tf.clip_by_norm(grad, clip_val), var)
     return optimizer.apply_gradients(gradients)
 
 
