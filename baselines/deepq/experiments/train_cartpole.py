02fb8de0470d9fb5338a97e4816339c7a64ce0db
ngc92 <ngc92@users.noreply.github.com>
2017-06-14 15:03:50 +0200

    don't exclude current epsiode when calculating 100 ep mean, fixes numpy warning

--- baselines/deepq/experiments/train_cartpole.py
+++ baselines/deepq/experiments/train_cartpole.py
@@ -5,7 +5,7 @@ from baselines import deepq
 
 def callback(lcl, glb):
     # stop training if reward exceeds 199
-    is_solved = lcl['t'] > 100 and sum(lcl['episode_rewards'][-101:-1]) / 100 >= 199
+    is_solved = lcl['t'] > 100 and sum(lcl['episode_rewards'][-100:]) / 100 >= 199
     return is_solved
 
 
