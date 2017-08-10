74d719a15e53497c97ea011522539b22827c35fd
Ashwin Reddy <ashwin.reddy338@gmail.com>
2017-08-10 02:30:23 -0700

    cartpole with saving

---
+++ baselines/pposgd/enjoy_cartpole.py
@@ -0,0 +1,38 @@
+# !/usr/bin/env python
+from baselines.common import set_global_seeds, tf_util as U
+from baselines import bench
+import os.path as osp
+import gym, logging
+from baselines import logger
+import sys
+import tensorflow as tf
+
+def enjoy(env_id, num_timesteps, seed):
+    from baselines.pposgd import mlp_policy, pposgd_simple
+    sess = U.make_session(num_cpu=1)
+    sess.__enter__()
+    logger.session().__enter__()
+    set_global_seeds(seed)
+    env = gym.make(env_id)
+    def policy_fn(name, ob_space, ac_space):
+        return mlp_policy.MlpPolicy(name=name, ob_space=ob_space, ac_space=ac_space,
+            hid_size=64, num_hid_layers=2)
+    env = bench.Monitor(env, osp.join(logger.get_dir(), "monitor.json"))
+    env.seed(seed)
+    gym.logger.setLevel(logging.WARN)
+    pi = policy_fn('pi', env.observation_space, env.action_space)
+    saver = tf.train.Saver()
+    with tf.Session() as ses:
+        saver.restore('/tmp/model')
+        done = False
+        while not done:
+            action = pi.act(True, obs)[0]
+            obs, reward, done, info = env.step(action)
+            env.render()
+
+def main():
+    enjoy('CartPole-v1', num_timesteps=1e6, seed=0)
+
+
+if __name__ == '__main__':
+    main()
---
+++ baselines/pposgd/run_cartpole.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+from baselines.common import set_global_seeds, tf_util as U
+from baselines import bench
+import os.path as osp
+import gym, logging
+from baselines import logger
+import sys
+import tensorflow as tf
+
+def train(env_id, num_timesteps, seed):
+    from baselines.pposgd import mlp_policy, pposgd_simple
+    sess = U.make_session(num_cpu=1)
+    sess.__enter__()
+    logger.session().__enter__()
+    set_global_seeds(seed)
+    env = gym.make(env_id)
+    def policy_fn(name, ob_space, ac_space):
+        return mlp_policy.MlpPolicy(name=name, ob_space=ob_space, ac_space=ac_space,
+            hid_size=64, num_hid_layers=2)
+    env = bench.Monitor(env, osp.join(logger.get_dir(), "monitor.json"))
+    env.seed(seed)
+    gym.logger.setLevel(logging.WARN)
+    pposgd_simple.learn(env, policy_fn,
+            max_timesteps=num_timesteps,
+            timesteps_per_batch=2048,
+            clip_param=0.2, entcoeff=0.0,
+            optim_epochs=10, optim_stepsize=3e-4, optim_batchsize=64,
+            gamma=0.99, lam=0.95,
+        )
+    env.close()
+    saver = tf.train.Saver()
+    saver.save(sess, '/tmp/model')
+
+def main():
+    print("Start")
+    train('CartPole-v1', num_timesteps=1e6, seed=0)
+
+if __name__ == "__main__":
+    main()
