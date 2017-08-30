88b7a8df8122b7c69edec89f239278646d695a6e
nchentanez <nchentanez@nvidia.com>
2017-08-30 16:57:58 +0700

    fix ddpg log bug and ppo reward print bug

--- baselines/ppo1/pposgd_simple.py
+++ baselines/ppo1/pposgd_simple.py
@@ -212,7 +212,8 @@ def learn(env, policy_func, *,
         lenbuffer.extend(lens)
         rewbuffer.extend(rews)
         logger.record_tabular("EpLenMean", np.mean(lenbuffer))
-        logger.record_tabular("EpRewMean", np.mean(rewbuffer))
+        rewmean = np.mean(rewbuffer)
+        logger.record_tabular("EpRewMean", rewmean)
         logger.record_tabular("EpThisIter", len(lens))
         episodes_so_far += len(lens)
         timesteps_so_far += sum(lens)
@@ -222,6 +223,7 @@ def learn(env, policy_func, *,
         logger.record_tabular("TimeElapsed", time.time() - tstart)
 
         if MPI.COMM_WORLD.Get_rank()==0:
+
             logF.write(str(rewmean) + "\n")
             logStats.write(logger.get_str() + "\n")
             logF.flush()
