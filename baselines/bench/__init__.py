c5c4b2847f2ba64e1ae840ad70edcca916055f96
smc77 <shane.conway@gmail.com>
2017-11-12 10:41:00 -0500

    Updated for deepmind function instead of deepq.

--- baselines/bench/__init__.py
+++ baselines/bench/__init__.py
@@ -1,3 +1,3 @@
 from baselines.bench.benchmarks import *
 from baselines.bench.monitor import *
-from baselines.bench.simple_bench import simple_bench
+#from baselines.bench.simple_bench import simple_bench
