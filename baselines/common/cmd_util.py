935049f9494331737a7a39f830fcb940e03fd03f
Ata Mahjoubfar <atamahjoubfar@users.noreply.github.com>
2018-02-07 15:41:10 -0800

    Fixed name of default MuJoCo environment
    Gym environment "Reacher-v1" is retired. So, if a MuJoCo environment is not specified in the arguments, and the code is run for the default environment, it would not work. To resolve the issue the default environment is changed to "Reacher-v2".

--- baselines/common/cmd_util.py
+++ baselines/common/cmd_util.py
@@ -58,7 +58,7 @@ def mujoco_arg_parser():
     Create an argparse.ArgumentParser for run_mujoco.py.
     """
     parser = arg_parser()
-    parser.add_argument('--env', help='environment ID', type=str, default="Reacher-v1")
+    parser.add_argument('--env', help='environment ID', type=str, default="Reacher-v2")
     parser.add_argument('--seed', help='RNG seed', type=int, default=0)
     parser.add_argument('--num-timesteps', type=int, default=int(1e6))
     return parser
