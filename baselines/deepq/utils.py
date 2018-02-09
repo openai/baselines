88a205b33ed04da16cc048bc468caa8800ed4456
bbueno5000 <bbueno5000@gmail.com>
2018-02-09 03:52:02 -0700

    copied scope functions from common/tf_util.py to deepq/util.py

--- baselines/deepq/utils.py
+++ baselines/deepq/utils.py
@@ -86,3 +86,33 @@ class Uint8Input(PlaceholderTfInput):
 
     def get(self):
         return self._output
+
+# ================================================================
+# Scopes
+# ================================================================
+
+def absolute_scope_name(relative_scope_name):
+    """Appends parent scope name to `relative_scope_name`"""
+    return scope_name() + "/" + relative_scope_name
+
+def scope_vars(scope, trainable_only=False):
+    """
+    Get variables inside a scope
+    The scope can be specified as a string
+
+    Parameters
+    ----------
+    scope: str or VariableScope
+        scope in which the variables reside.
+    trainable_only: bool
+        whether or not to return only the variables that were marked as trainable.
+
+    Returns
+    -------
+    vars: [tf.Variable]
+        list of variables in `scope`.
+    """
+    return tf.get_collection(
+        tf.GraphKeys.TRAINABLE_VARIABLES if trainable_only else tf.GraphKeys.GLOBAL_VARIABLES,
+        scope=scope if isinstance(scope, str) else scope.name
+    )
