e82b242f0e6e10e9036de80ac90a7f21a0707c11
Roy Fox <roy.d.fox@gmail.com>
2018-04-21 14:10:18 -0700

    arr[end] was excluded when end is not None

--- baselines/common/segment_tree.py
+++ baselines/common/segment_tree.py
@@ -68,10 +68,9 @@ class SegmentTree(object):
             result of reducing self.operation over the specified range of array elements.
         """
         if end is None:
-            end = self._capacity
+            end = self._capacity - 1
         if end < 0:
             end += self._capacity
-        end -= 1
         return self._reduce_helper(start, end, 1, 0, self._capacity - 1)
 
     def __setitem__(self, idx, val):
