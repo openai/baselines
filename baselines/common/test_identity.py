4dc709faae955dd95154718bbd3f002075202038
Peter Zhokhov <peterz@openai.com>
2018-05-21 14:46:49 -0700

    proper rng seeding in the test_identity

--- baselines/common/test_identity.py
+++ baselines/common/test_identity.py
@@ -1,6 +1,8 @@
 import pytest
 import tensorflow as tf
 import random
+import numpy as np
+from gym.spaces import np_random
 
 from baselines.a2c import a2c
 from baselines.ppo2 import ppo2
@@ -11,7 +13,7 @@ from baselines.ppo2.policies import MlpPolicy
 
 learn_func_list = [
     lambda e: a2c.learn(policy=MlpPolicy, env=e, seed=0, total_timesteps=50000),
-    lambda e: ppo2.learn(policy=MlpPolicy, env=e, total_timesteps=50000, lr=1e-2, nsteps=128, ent_coef=0.01)
+    lambda e: ppo2.learn(policy=MlpPolicy, env=e, total_timesteps=50000, lr=1e-3, nsteps=128, ent_coef=0.01)
 ]
 
 
@@ -22,9 +24,11 @@ def test_identity(learn_func):
     Test if the algorithm (with a given policy) 
     can learn an identity transformation (i.e. return observation as an action)
     '''
+    np.random.seed(0)
+    np_random.seed(0)
+    random.seed(0)
 
     env = DummyVecEnv([lambda: IdentityEnv(10)])
-    random.seed(0)
 
     with tf.Graph().as_default(), tf.Session().as_default():
         tf.set_random_seed(0)
