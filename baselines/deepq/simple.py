7c9278984cfcd6eb28627a4d3eb6e7834f0cdc4f
ashley <ashley.hill.1993@gmail.com>
2018-07-09 13:53:12 +0200

    fixed deepq empty slice warning

--- baselines/deepq/simple.py
+++ baselines/deepq/simple.py
@@ -284,7 +284,11 @@ def learn(env,
                 # Update target network periodically.
                 update_target(sess=act.sess)
 
-            mean_100ep_reward = round(float(np.mean(episode_rewards[-101:-1])), 1)
+            if len(episode_rewards[-101:-1]) == 0:
+                mean_100ep_reward = -np.inf
+            else:
+                mean_100ep_reward = round(float(np.mean(episode_rewards[-101:-1])), 1)
+
             num_episodes = len(episode_rewards)
             if done and print_freq is not None and len(episode_rewards) % print_freq == 0:
                 logger.record_tabular("steps", t)
