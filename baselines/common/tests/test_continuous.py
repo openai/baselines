f7027474f9c065a82ff994fd6c4eb79a5c2def03
Antonin RAFFIN <antonin.raffin@ensta-paristech.fr>
2018-07-11 13:06:18 +0200

    Add test for ddpg

---
+++ baselines/common/tests/test_continuous.py
@@ -0,0 +1,19 @@
+import subprocess
+
+import pytest
+
+ENV_ID = 'Pendulum-v0'
+
+def _assert_eq(left, right):
+    assert left == right, '{} != {}'.format(left, right)
+
+
+def _assert_neq(left, right):
+    assert left != right, '{} == {}'.format(left, right)
+
+
+def test_ddpg():
+    args = ['--env-id', ENV_ID, '--nb-epochs', 2, '--nb-epoch-cycles', 2, '--nb-rollout-steps', 100]
+    args = list(map(str, args))
+    ok = subprocess.call(['python', '-m', 'baselines.ddpg.main'] + args)
+    _assert_eq(ok, 0)
