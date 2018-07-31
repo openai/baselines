10f815fe1d41ccbf37227120e22be4882b1d4268
Peter Zhokhov <peterz@openai.com>
2018-07-31 15:19:43 -0700

    fixed import in vec_normalize

--- baselines/common/vec_env/vec_normalize.py
+++ baselines/common/vec_env/vec_normalize.py
@@ -1,5 +1,5 @@
 from baselines.common.vec_env import VecEnvWrapper
-from baselines.common.running_mean_std import RunningMeanStd
+from baselines.common.running_mean_std import TfRunningMeanStd
 import numpy as np
 
 class VecNormalize(VecEnvWrapper):
