ac4f940e44a9b4c174d2cbee34b262e09f2a7fa3
ashley <ashley.hill.1993@gmail.com>
2018-08-16 17:20:39 +0200

    fixed memorized vectorized if environment

--- baselines/ppo2/ppo2.py
+++ baselines/ppo2/ppo2.py
@@ -277,7 +277,8 @@ class PPO2(BaseRLModel):
             "policy": self.policy,
             "observation_space": self.observation_space,
             "action_space": self.action_space,
-            "n_envs": self.n_envs
+            "n_envs": self.n_envs,
+            "_vectorize_action": self._vectorize_action
         }
 
         params = self.sess.run(self.params)
