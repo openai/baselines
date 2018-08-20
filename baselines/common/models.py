caf7b08b4d47a53b6abb77477144ac355cdb4a05
pzhokhov <peterzhokhoff@gmail.com>
2018-08-20 13:54:56 -0700

    Baselines issue #525 (lack of docs for recurrent policies) (#27)
    * make nminibatces = min(nminibatches, nenv)
    
    * clarify the usage of lstm policy, add an example and a test
    
    * cleaned up example, added assert to the test
    
    * remove nminibatches -> min(nminibatches, num_env)
    
    * removed code snippet from the docstring, pointing to the file

--- baselines/common/models.py
+++ baselines/common/models.py
@@ -22,8 +22,7 @@ def nature_cnn(unscaled_images, **conv_kwargs):
 
 def mlp(num_layers=2, num_hidden=64, activation=tf.tanh):
     """
-    Simple fully connected layer policy. Separate stacks of fully-connected layers are used for policy and value function estimation.
-    More customized fully-connected policies can be obtained by using PolicyWithV class directly.
+    Stack of fully-connected layers to be used in a policy / q-function approximator
 
     Parameters:
     ----------
@@ -37,7 +36,7 @@ def mlp(num_layers=2, num_hidden=64, activation=tf.tanh):
     Returns:
     -------
 
-    function that builds fully connected network with a given input placeholder
+    function that builds fully connected network with a given input tensor / placeholder
     """        
     def network_fn(X):
         h = tf.layers.flatten(X)
@@ -68,6 +67,34 @@ def cnn_small(**conv_kwargs):
 
 
 def lstm(nlstm=128, layer_norm=False):
+    """
+    Builds LSTM (Long-Short Term Memory) network to be used in a policy.
+    Note that the resulting function returns not only the output of the LSTM 
+    (i.e. hidden state of lstm for each step in the sequence), but also a dictionary
+    with auxiliary tensors to be set as policy attributes. 
+
+    Specifically, 
+        S is a placeholder to feed current state (LSTM state has to be managed outside policy)
+        M is a placeholder for the mask (used to mask out observations after the end of the episode, but can be used for other purposes too)
+        initial_state is a numpy array containing initial lstm state (usually zeros)
+        state is the output LSTM state (to be fed into S at the next call)
+
+
+    An example of usage of lstm-based policy can be found here: common/tests/test_doc_examples.py/test_lstm_example
+            
+    Parameters:
+    ----------
+
+    nlstm: int          LSTM hidden state size
+
+    layer_norm: bool    if True, layer-normalized version of LSTM is used
+
+    Returns:
+    -------
+
+    function that builds LSTM with a given input tensor / placeholder
+    """
+        
     def network_fn(X, nenv=1):
         nbatch = X.shape[0] 
         nsteps = nbatch // nenv
---
+++ baselines/common/tests/test_doc_examples.py
@@ -0,0 +1,35 @@
+def test_lstm_example():
+    import tensorflow as tf
+    from baselines.common import policies, models, cmd_util
+    from baselines.common.vec_env.dummy_vec_env import DummyVecEnv
+
+    # create vectorized environment
+    venv = DummyVecEnv([lambda: cmd_util.make_mujoco_env('Reacher-v2', seed=0)])
+
+    with tf.Session() as sess:
+        # build policy based on lstm network with 128 units
+        policy = policies.build_policy(venv, models.lstm(128))(nbatch=1, nsteps=1)
+
+        # initialize tensorflow variables
+        sess.run(tf.global_variables_initializer())
+
+        # prepare environment variables
+        ob = venv.reset()
+        state = policy.initial_state
+        done = [False]
+        step_counter = 0
+
+        # run a single episode until the end (i.e. until done)
+        while True:
+            action, _, state, _ = policy.step(ob, S=state, M=done)
+            ob, reward, done, _ = venv.step(action)
+            step_counter += 1
+            if done:    
+                break
+
+        
+        assert step_counter > 5
+            
+
+
+
