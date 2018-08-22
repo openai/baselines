a29ee8816b116bd0945da05ada4803d5f8edfa15
Damien Lancry <drl17@graphic08.doc.ic.ac.uk>
2018-08-22 21:41:52 +0100

    I investigated a bit about running a training in a vectorized monitored mujoco env and found out that the 0.monitor.csv file could not be plotted using baselines.results_plotter.py functions. Moreover the seed is the same in every parallel environments due to the particular behaviour of lambda. this fixes both issues without breaking the function in other files (baselines.acktr.run_mujoco still works)

--- baselines/common/cmd_util.py
+++ baselines/common/cmd_util.py
@@ -32,15 +32,15 @@ def make_atari_env(env_id, num_env, seed, wrapper_kwargs=None, start_index=0):
     set_global_seeds(seed)
     return SubprocVecEnv([make_env(i + start_index) for i in range(num_env)])
 
-def make_mujoco_env(env_id, seed, reward_scale=1.0):
+def make_mujoco_env(env_id, seed, i=0, reward_scale=1.0):
     """
     Create a wrapped, monitored gym.Env for MuJoCo.
     """
     rank = MPI.COMM_WORLD.Get_rank()
-    myseed = seed  + 1000 * rank if seed is not None else None
+    myseed = seed + i + 1000 * rank if seed is not None else None
     set_global_seeds(myseed)
     env = gym.make(env_id)
-    logger_path = None if logger.get_dir() is None else os.path.join(logger.get_dir(), str(rank))
+    logger_path = None if logger.get_dir() is None else os.path.join(logger.get_dir(), str(rank)+ '.' + str(i))
     env = Monitor(env, logger_path, allow_early_resets=True)
     env.seed(seed)
 
