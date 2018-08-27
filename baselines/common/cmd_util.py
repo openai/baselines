f6dcb51a1818e2ca0f2ab7fc1547d98928ce224f
Damien Lancry <drl17@edge04.doc.ic.ac.uk>
2018-08-27 22:12:34 +0100

    fix if else

--- baselines/common/cmd_util.py
+++ baselines/common/cmd_util.py
@@ -30,7 +30,7 @@ def make_vec_env(env_id, env_type, num_env, seed, wrapper_kwargs=None, start_ind
             env = make_atari(env_id) if env_type == 'atari' else gym.make(env_id)
             env.seed(seed + 10000*mpi_rank + rank if seed is not None else None)
             env = Monitor(env, logger.get_dir() and os.path.join(logger.get_dir(), str(mpi_rank) + '.' + str(rank)))
-            if env_type == 'atari': return wrap_deepmind(env, **wrapper_kwargs) if env_type == 'atari' else env
+            if env_type == 'atari': return wrap_deepmind(env, **wrapper_kwargs)
             elif reward_scale != 1: return RewardScaler(env, reward_scale)
             else: return env
         return _thunk
