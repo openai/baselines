c1bb5c30b5a5494861b17a3bbbeb19dcd0bbf8b3
Antonin RAFFIN <antonin.raffin@ensta-paristech.fr>
2018-09-05 15:40:56 +0200

    Bug fixes
    Known issue: DDPG test does not pass

--- tests/test_identity.py
+++ tests/test_identity.py
@@ -13,6 +13,7 @@ from stable_baselines.common.vec_env.dummy_vec_env import DummyVecEnv
 from stable_baselines.common.policies import MlpPolicy
 from stable_baselines.deepq import models as deepq_models
 
+
 learn_func_list = [
     lambda e: A2C(policy=MlpPolicy, learning_rate=1e-3, n_steps=1,
                   gamma=0.7, env=e).learn(total_timesteps=10000, seed=0),
@@ -21,7 +22,7 @@ learn_func_list = [
     lambda e: ACKTR(policy=MlpPolicy, env=e, learning_rate=5e-4, n_steps=1).learn(total_timesteps=20000, seed=0),
     lambda e: DeepQ(policy=deepq_models.mlp([32]), batch_size=16, gamma=0.1,
                     exploration_fraction=0.001, env=e).learn(total_timesteps=40000, seed=0),
-    lambda e: PPO1(policy=MlpPolicy, env=e, lam=0.7,
+    lambda e: PPO1(policy=MlpPolicy, env=e, lam=0.5,
                    optim_batchsize=16, optim_stepsize=1e-3).learn(total_timesteps=15000, seed=0),
     lambda e: PPO2(policy=MlpPolicy, env=e, learning_rate=1.5e-3,
                    lam=0.8).learn(total_timesteps=20000, seed=0),
@@ -60,8 +61,9 @@ def test_identity_ddpg():
     Test if the algorithm (with a given policy)
     can learn an identity transformation (i.e. return observation as an action)
     """
-    env = DummyVecEnv([lambda: IdentityEnvBox(10)])
+    env = DummyVecEnv([lambda: IdentityEnvBox()])
 
+    # FIXME: this test fail for now
     model = DDPG(MlpPolicy, env).learn(total_timesteps=10000, seed=0)
 
     n_trials = 1000
