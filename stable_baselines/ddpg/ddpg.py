fixed scaling action

stable_baselines/ddpg/ddpg.py
@@ -560,7 +560,7 @@ class DDPG(BaseRLModel):
             noise = self.action_noise()
             assert noise.shape == action.shape
             action += noise
-        action = np.clip(action, self.action_space.low, self.action_space.high)
+        action = np.clip(action, -1, 1)
         return action, q_value
 
     def _store_transition(self, obs0, action, reward, obs1, terminal1):
@@ -786,7 +786,7 @@ class DDPG(BaseRLModel):
                             # Execute next action.
                             if rank == 0 and self.render:
                                 self.env.render()
-                            new_obs, reward, done, _ = self.env.step(action)
+                            new_obs, reward, done, _ = self.env.step(action * np.abs(self.action_space.low))
 
                             if writer is not None:
                                 ep_rew = np.array([reward]).reshape((1, -1))
@@ -853,7 +853,8 @@ class DDPG(BaseRLModel):
                                     return self
 
                                 eval_action, eval_q = self._policy(eval_obs, apply_noise=False, compute_q=True)
-                                eval_obs, eval_r, eval_done, _ = self.eval_env.step(eval_action)
+                                eval_obs, eval_r, eval_done, _ = self.eval_env.step(eval_action *
+                                                                                    np.abs(self.action_space.low))
                                 if self.render_eval:
                                     self.eval_env.render()
                                 eval_episode_reward += eval_r
@@ -933,6 +934,7 @@ class DDPG(BaseRLModel):
         observation = np.array(observation).reshape(self.observation_space.shape)
 
         action, _ = self._policy(observation, apply_noise=False, compute_q=True)
+        action = action * np.abs(self.action_space.low)  # scale the output for the prediction
         if self._vectorize_action:
             return [action], [None]
         else:

stable_baselines/ddpg/policies.py
@@ -135,10 +135,9 @@ class FeedForwardPolicy(DDPGPolicy):
                 if self.layer_norm:
                     pi_h = tf.contrib.layers.layer_norm(pi_h, center=True, scale=True)
                 pi_h = self.activ(pi_h)
-            pi_latent = tf.nn.tanh(tf.layers.dense(pi_h, self.ac_space.shape[0], name=scope,
-                                                   kernel_initializer=tf.random_uniform_initializer(minval=-3e-3,
-                                                                                                    maxval=3e-3)))
-            self.policy = tf.multiply(pi_latent, tf.convert_to_tensor(np.abs(self.ac_space.low)))
+            self.policy = tf.nn.tanh(tf.layers.dense(pi_h, self.ac_space.shape[0], name=scope,
+                                                     kernel_initializer=tf.random_uniform_initializer(minval=-3e-3,
+                                                                                                      maxval=3e-3)))
         return self.policy
 
     def make_critic(self, obs=None, action=None, reuse=False, scope="qf"):