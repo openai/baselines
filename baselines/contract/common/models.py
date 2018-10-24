c6422b684995cac8332118f9172fccef575eccf4
Eleanor Quint <pquint@cse.unl.edu>
2018-10-24 11:50:49 -0500

    Attempt to make contract state augmentation work better

--- baselines/contract/common/models.py
+++ baselines/contract/common/models.py
@@ -10,8 +10,9 @@ def augment_network_with_contract_state(base_network):
         contracts = placeholders[1:]
 
         h_main = layers.flatten(base_network(unscaled_images, **conv_kwargs))
+        h_main = fc(h_main, 'fc_hmain', nh=512)
         h_cont = [
-            fc(c, 'fc_' + str(i), nh=4, init_scale=np.sqrt(2)) for i, c in enumerate(contracts)
+            fc(c, 'fc_' + str(i), nh=4) for i, c in enumerate(contracts)
         ]
         return tf.concat([h_main] + h_cont, axis=-1)
     return network
