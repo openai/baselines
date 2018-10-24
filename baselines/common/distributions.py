bfe971ed9531b51ba6ab5010787c31115b8222ab
Andy Twigg <andy.twigg@gmail.com>
2018-10-24 10:11:55 -0700

    minor changes

--- baselines/common/distributions.py
+++ baselines/common/distributions.py
@@ -39,7 +39,7 @@ class PdType(object):
         raise NotImplementedError
     def pdfromflat(self, flat):
         return self.pdclass()(flat)
-    def pdfromlatent(self, latent_vector):
+    def pdfromlatent(self, latent_vector, init_scale, init_bias):
         raise NotImplementedError
     def param_shape(self):
         raise NotImplementedError
@@ -81,7 +81,7 @@ class MultiCategoricalPdType(PdType):
     def pdfromflat(self, flat):
         return MultiCategoricalPd(self.ncats, flat)
     def pdfromlatent(self, latent_vector, init_scale=1.0, init_bias=0.0):
-        pdparam = fc(latent_vector, 'pi', sum(self.ncats), init_scale=init_scale, init_bias=init_bias)
+        pdparam = fc(latent_vector, 'pi', self.ncats.sum(), init_scale=init_scale, init_bias=init_bias)
         return self.pdfromflat(pdparam), pdparam
     def param_shape(self):
         return [sum(self.ncats)]
