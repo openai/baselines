58f8e1dd5315a3ea32ac393cb261c4a557b2d6e3
Eleanor Quint <pquint@cse.unl.edu>
2018-10-30 17:03:39 -0500

    Expand allowed environments to include Deterministic

--- baselines/common/atari_wrappers.py
+++ baselines/common/atari_wrappers.py
@@ -215,7 +215,7 @@ class LazyFrames(object):
 
 def make_atari(env_id):
     env = gym.make(env_id)
-    assert 'NoFrameskip' in env.spec.id
+    assert 'NoFrameskip' or 'Deterministic' in env.spec.id
     env = NoopResetEnv(env, noop_max=30)
     env = MaxAndSkipEnv(env, skip=4)
     return env
