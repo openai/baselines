2b95e1462f21c6dd64ad252420de8e7d3bb9d548
Mathieu Poliquin <mathieu.poliquin@gmail.com>
2018-10-30 21:28:54 +0800

    ppo2 retro defaults to atari

--- baselines/ppo2/defaults.py
+++ baselines/ppo2/defaults.py
@@ -22,10 +22,4 @@ def atari():
     )
 
 def retro():
-    return dict(
-        nsteps=128, nminibatches=4,
-        lam=0.95, gamma=0.99, noptepochs=4, log_interval=1,
-        ent_coef=.01,
-        lr=lambda f : f * 2.5e-4,
-        cliprange=lambda f : f * 0.1,
-    )
+    return atari()
