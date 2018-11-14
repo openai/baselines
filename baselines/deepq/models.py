339d5640b9f54fa57f78232d0161bfe0c7eec90d
Roman Ring <inoryy@gmail.com>
2018-11-14 22:22:42 +0200

    add docs for layer_norm param in DQN baseline (#107)

--- baselines/deepq/models.py
+++ baselines/deepq/models.py
@@ -21,6 +21,9 @@ def mlp(hiddens=[], layer_norm=False):
     ----------
     hiddens: [int]
         list of sizes of hidden layers
+    layer_norm: bool
+        if true applies layer normalization for every layer
+        as described in https://arxiv.org/abs/1607.06450
 
     Returns
     -------
@@ -80,6 +83,9 @@ def cnn_to_mlp(convs, hiddens, dueling=False, layer_norm=False):
     dueling: bool
         if true double the output MLP to compute a baseline
         for action scores
+    layer_norm: bool
+        if true applies layer normalization for every layer
+        as described in https://arxiv.org/abs/1607.06450
 
     Returns
     -------
