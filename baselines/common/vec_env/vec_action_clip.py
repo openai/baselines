e81251bc700659bbf86820da8bf37cedec1e859c
Xingdong Zuo <zuoxingdong@users.noreply.github.com>
2018-11-15 15:34:11 +0100

    Update vec_action_clip.py

--- baselines/common/vec_env/vec_action_clip.py
+++ baselines/common/vec_env/vec_action_clip.py
@@ -7,11 +7,11 @@ class VecActionClip(VecEnvWrapper):
         low = self.action_space.low
         high = self.action_space.high
         actions = [np.clip(action, low, high) for action in actions]
-        
+
         self.venv.step_async(actions)
-    
+
     def step_wait(self):
         return self.venv.step_wait()
-    
+
     def reset(self):
         return self.venv.reset()
