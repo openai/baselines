fbc54a4afb607c3e36d0cc180c654e051b1c6492
Andy Twigg <andy.twigg@gmail.com>
2018-11-16 11:19:33 -0800

    setup.py fix

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(name='baselines',
       packages=[package for package in find_packages()
                 if package.startswith('baselines')],
       install_requires=[
-          'gym[mujoco,atari,classic_control,robotics]',
+          'gym',
           'scipy',
           'tqdm',
           'joblib',
