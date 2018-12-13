021fbe9c84f47a436327ac758d448a91c4d3e939
Yoshimura Naoya <yesyes708@gmail.com>
2018-12-13 16:01:01 +0900

    remove unnecessary logs

--- baselines/her/experiment/train.py
+++ baselines/her/experiment/train.py
@@ -50,8 +50,8 @@ def train(policy, rollout_worker, evaluator,
         for _ in range(n_cycles):
             episode = rollout_worker.generate_rollouts()
             clogger.info("Episode = {}".format(episode.keys()))
-            for key in episode.keys():
-                clogger.info(" - {}: {}".format(key, episode[key].shape))
+            # for key in episode.keys():
+            #     clogger.info(" - {}: {}".format(key, episode[key].shape))
             policy.store_episode(episode)
             for _ in range(n_batches):
                 policy.train()
