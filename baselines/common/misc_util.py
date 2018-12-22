e6abbc6dab2dc79910d267d55bb70e05bb511041
Xingdong Zuo <zuoxingdong@users.noreply.github.com>
2018-12-22 19:05:15 +0100

    Update misc_util.py

--- baselines/common/misc_util.py
+++ baselines/common/misc_util.py
@@ -7,6 +7,12 @@ import tempfile
 import zipfile
 
 
+def zipsame(*seqs):
+    L = len(seqs[0])
+    assert all(len(seq) == L for seq in seqs[1:])
+    return zip(*seqs)
+
+
 class EzPickle(object):
     """Objects that are pickled and unpickled via their constructor
     arguments.
