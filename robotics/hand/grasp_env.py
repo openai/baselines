569fdc9a4cdc4e732f48075bb9f7cfdc683c1777
Unknown <h.kazuki.lovebomb@gmail.com>
2019-01-07 17:30:10 +0900

    test to moving hand by mujoco-py

--- robotics/hand/grasp_env.py
+++ robotics/hand/grasp_env.py
@@ -3,6 +3,7 @@
 Displays robot fetch at a disco party.
 """
 from mujoco_py import load_model_from_path, MjSim, MjViewer
+import math
 import os
 
 model = load_model_from_path("../assets/hand/grasp_block.xml")
@@ -15,5 +16,12 @@ t = 0
 while True:
     viewer.render()
     t += 1
-    if t > 100 and os.getenv('TESTING') is not None:
-        break
+    sim.step()
+    state = sim.get_state()
+
+    state.qpos[1] = 0.1*math.sin(0.01*t)
+    state.qpos[0] = 0.1*math.cos(0.01*t)
+
+    sim.set_state(state)
+    # if t > 100 and os.getenv('TESTING') is not None:
+    #     break
