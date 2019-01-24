bb05b9ee8800bcdbb7f0f8316a75dd6868695e70
Peter Zhokhov <peterz@openai.com>
2019-01-24 14:27:22 -0800

    removed unnecessary OrderedDict requirement in subproc_vec_env

--- baselines/common/vec_env/subproc_vec_env.py
+++ baselines/common/vec_env/subproc_vec_env.py
@@ -112,7 +112,6 @@ def _flatten_obs(obs):
 
     if isinstance(obs[0], dict):
         import collections
-        assert isinstance(obs, collections.OrderedDict)
         keys = obs[0].keys()
         return {k: np.stack([o[k] for o in obs]) for k in keys}
     else:
