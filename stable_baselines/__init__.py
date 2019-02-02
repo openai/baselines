b135a81523c70ced32a452a330e64a09a5539421
Antonin RAFFIN <antonin.raffin@ensta.org>
2019-02-02 16:29:26 +0100

    Bump version + remove gym spaces patch

--- stable_baselines/__init__.py
+++ stable_baselines/__init__.py
@@ -1,6 +1,3 @@
-import gym
-import numpy as np
-
 from stable_baselines.a2c import A2C
 from stable_baselines.acer import ACER
 from stable_baselines.acktr import ACKTR
@@ -12,19 +9,4 @@ from stable_baselines.ppo2 import PPO2
 from stable_baselines.trpo_mpi import TRPO
 from stable_baselines.sac import SAC
 
-__version__ = "2.4.0"
-
-
-# patch Gym spaces to add equality functions, if not implemented
-# See https://github.com/openai/gym/issues/1171
-if gym.spaces.MultiBinary.__eq__ == object.__eq__:  # by default, all classes have the __eq__ function from object.
-    def _eq(self, other):
-        return self.n == other.n
-
-    gym.spaces.MultiBinary.__eq__ = _eq
-
-if gym.spaces.MultiDiscrete.__eq__ == object.__eq__:
-    def _eq(self, other):
-        return np.all(self.nvec == other.nvec)
-
-    gym.spaces.MultiDiscrete.__eq__ = _eq
+__version__ = "2.4.1a"
