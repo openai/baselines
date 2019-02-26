5c7da772a48636b4d7d552b951b35ad8b67fbeb5
Peter Zhokhov <peterz@openai.com>
2019-02-26 15:44:26 -0800

    Merge branch 'master' of github.com:openai/games
     the commit.

--- baselines/common/vec_env/vec_remove_dict_obs.py
+++ baselines/common/vec_env/vec_remove_dict_obs.py
@@ -1,22 +1,11 @@
-from .vec_env import VecEnvWrapper
+from .vec_env import VecEnvObservationWrapper
 
 
-class VecRemoveDictObs(VecEnvWrapper):
-    """
-    PPO2 doesn't support dictionary observations, so make the environment only expose the observation for the provided key.
-    """
-
+class VecExtractDictObs(VecEnvObservationWrapper):
     def __init__(self, venv, key):
-        self._key = key
-        self._venv = venv
-        super().__init__(venv, observation_space=venv.observation_space.spaces[self._key])
+        self.key = key
+        super().__init__(venv=venv,
+            observation_space=venv.observation_space.spaces[self.key])
 
-    def _remove_dict(self, obs):
-        return obs[self._key]
-
-    def reset(self):
-        return self._remove_dict(self._venv.reset())
-
-    def step_wait(self):
-        obs, rews, dones, infos = self._venv.step_wait()
-        return self._remove_dict(obs), rews, dones, infos
+    def process(self, obs):
+        return obs[self.key]
