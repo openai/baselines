Fixed DDPG normalization of obs and returns (#230)
* fix ddpg normalization of obs and returns

* Update DDPG default buffer size

* Add test for DDPG normalization

stable_baselines/ddpg/ddpg.py
@@ -177,7 +177,7 @@ class DDPG(OffPolicyRLModel):
                  normalize_observations=False, tau=0.001, batch_size=128, param_noise_adaption_interval=50,
                  normalize_returns=False, enable_popart=False, observation_range=(-5., 5.), critic_l2_reg=0.,
                  return_range=(-np.inf, np.inf), actor_lr=1e-4, critic_lr=1e-3, clip_norm=None, reward_scale=1.,
-                 render=False, render_eval=False, memory_limit=100, verbose=0, tensorboard_log=None,
+                 render=False, render_eval=False, memory_limit=50000, verbose=0, tensorboard_log=None,
                  _init_setup_model=True, policy_kwargs=None, full_tensorboard_log=False):
 
         # TODO: replay_buffer refactoring
@@ -394,6 +394,10 @@ class DDPG(OffPolicyRLModel):
 
                 self.params = find_trainable_variables("model")
                 self.target_params = find_trainable_variables("target")
+                self.obs_rms_params = [var for var in tf.global_variables()
+                                           if "obs_rms" in var.name]
+                self.ret_rms_params = [var for var in tf.global_variables()
+                                           if "ret_rms" in var.name]
 
                 with self.sess.as_default():
                     self._initialize(self.sess)
@@ -1012,8 +1016,17 @@ class DDPG(OffPolicyRLModel):
 
         params = self.sess.run(self.params)
         target_params = self.sess.run(self.target_params)
+        norm_obs_params = self.sess.run(self.obs_rms_params)
+        norm_ret_params = self.sess.run(self.ret_rms_params)
 
-        self._save_to_file(save_path, data=data, params=params + target_params)
+        params_to_save = params \
+            + target_params \
+            + norm_obs_params \
+            + norm_ret_params
+        self._save_to_file(save_path,
+                           data=data,
+                           params=params_to_save)
+
 
     @classmethod
     def load(cls, load_path, env=None, **kwargs):
@@ -1031,7 +1044,11 @@ class DDPG(OffPolicyRLModel):
         model.setup_model()
 
         restores = []
-        for param, loaded_p in zip(model.params + model.target_params, params):
+        params_to_load = model.params \
+            + model.target_params \
+            + model.obs_rms_params \
+            + model.ret_rms_params
+        for param, loaded_p in zip(params_to_load, params):
             restores.append(param.assign(loaded_p))
         model.sess.run(restores)
 

tests/test_continuous.py
@@ -16,6 +16,7 @@ from stable_baselines.trpo_mpi import TRPO
 from stable_baselines.common import set_global_seeds
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines.common.identity_env import IdentityEnvBox
+from stable_baselines.ddpg import AdaptiveParamNoiseSpec
 from tests.test_common import _assert_eq
 
 
@@ -149,7 +150,7 @@ def test_model_manipulation(model_class):
 
 
 def test_ddpg():
-    args = ['--env-id', 'Pendulum-v0', '--num-timesteps', 1000]
+    args = ['--env-id', 'Pendulum-v0', '--num-timesteps', 1000, '--noise-type', 'ou_0.01']
     args = list(map(str, args))
     return_code = subprocess.call(['python', '-m', 'stable_baselines.ddpg.main'] + args)
     _assert_eq(return_code, 0)
@@ -165,3 +166,30 @@ def test_ddpg_eval_env():
                 nb_train_steps=2, nb_eval_steps=10,
                 eval_env=eval_env, verbose=0)
     model.learn(1000)
+
+
+def test_ddpg_normalization():
+    """
+    Test that observations and returns normalizations are properly saved and loaded.
+    """
+    param_noise = AdaptiveParamNoiseSpec(initial_stddev=0.05, desired_action_stddev=0.05)
+    model = DDPG('MlpPolicy', 'Pendulum-v0', memory_limit=50000, normalize_observations=True,
+                 normalize_returns=True, nb_rollout_steps=128, nb_train_steps=1,
+                 batch_size=64, param_noise=param_noise)
+    model.learn(1000)
+    obs_rms_params = model.sess.run(model.obs_rms_params)
+    ret_rms_params = model.sess.run(model.ret_rms_params)
+    model.save('./test_ddpg')
+
+    loaded_model = DDPG.load("test_ddpg")
+    obs_rms_params_2 = loaded_model.sess.run(loaded_model.obs_rms_params)
+    ret_rms_params_2 = loaded_model.sess.run(loaded_model.ret_rms_params)
+
+    for param, param_loaded in zip(obs_rms_params + ret_rms_params,
+                                   obs_rms_params_2 + ret_rms_params_2):
+        assert np.allclose(param, param_loaded)
+
+    del model, loaded_model
+
+    if os.path.exists("./test_ddpg"):
+        os.remove("./test_ddpg")