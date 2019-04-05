c79b3373bf4fa276fa05151494f4777c0d8f8a36
Peter Zhokhov <peterz@openai.com>
2019-04-05 14:43:09 -0700

    parse colon-separated env_id's

--- baselines/common/cmd_util.py
+++ baselines/common/cmd_util.py
@@ -150,7 +150,6 @@ def common_arg_parser():
     parser.add_argument('--save_video_interval', help='Save video every x steps (0 = disabled)', default=0, type=int)
     parser.add_argument('--save_video_length', help='Length of recorded video. Default: 200', default=200, type=int)
     parser.add_argument('--play', default=False, action='store_true')
-    parser.add_argument('--extra_import', help='Extra module to import to access external environments', type=str, default=None)
     return parser
 
 def robotics_arg_parser():
