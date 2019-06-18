17cd1f3a9c3bd3aaf96931e91fd7465432219d01
gagan <gk2496@columbia.edu>
2019-06-18 19:34:57 -0400

    building policy with Dict ob_space + DictInputWrapper

--- baselines/common/input.py
+++ baselines/common/input.py
@@ -1,6 +1,6 @@
 import numpy as np
 import tensorflow as tf
-from gym.spaces import Discrete, Box, MultiDiscrete
+from gym.spaces import Discrete, Box, MultiDiscrete, Dict
 
 def observation_placeholder(ob_space, batch_size=None, name='Ob'):
     '''
@@ -21,8 +21,8 @@ def observation_placeholder(ob_space, batch_size=None, name='Ob'):
     tensorflow placeholder tensor
     '''
 
-    assert isinstance(ob_space, Discrete) or isinstance(ob_space, Box) or isinstance(ob_space, MultiDiscrete), \
-        'Can only deal with Discrete and Box observation spaces for now'
+    assert isinstance(ob_space, Discrete) or isinstance(ob_space, Box) or isinstance(ob_space, MultiDiscrete) \
+        or isinstance(ob_space, Dict), 'Can only deal with Discrete, Box, Dict observation spaces for now'
 
     dtype = ob_space.dtype
     if dtype == np.int8:
@@ -59,6 +59,8 @@ def encode_observation(ob_space, placeholder):
         placeholder = tf.cast(placeholder, tf.int32)
         one_hots = [tf.to_float(tf.one_hot(placeholder[..., i], ob_space.nvec[i])) for i in range(placeholder.shape[-1])]
         return tf.concat(one_hots, axis=-1)
+    elif isinstance(ob_space, Dict):
+        return tf.to_float(placeholder)  # same as Box, must wrap env with gym.wrappers.DictInputWrapper
     else:
         raise NotImplementedError
 
