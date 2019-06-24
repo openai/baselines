89a30a471c98374e09dfa0965986df0c3e66bf5c
Gagan <gagan.khandate@columbia.edu>
2019-06-24 14:45:50 -0400

    Trajectory utility methods

--- baselines/ppo_her/traj_util.py
+++ baselines/ppo_her/traj_util.py
@@ -6,9 +6,32 @@ class Trajectory(object):
     def __init__(self):
         self.obs = []
         self.actions = []
+        self.rewards = []
+        self.done = False
 
     def __len__(self):
         return len(self.actions)
+
+    def append(self, step):
+        """ append step """
+        obs, action, reward = step
+        self.obs.append(obs)
+        self.actions.append(action)
+        self.rewards.append(reward)
+
+    def pop(self):
+        """ pop step """
+        return self.obs.pop(), self.actions.pop(), self.rewards.pop()
+
+    def update_obs(self, key, value):
+        for obs in self.obs:
+            obs[key] = value
+
+    def update_rewards(self, compute_reward):
+        assert callable(compute_reward)
+        for t in range(len(self)):
+            action, obs = self.actions[t], self.obs[t+1]
+            self.rewards[t] = compute_reward(obs['achieved_goal'], obs['desired_goal'], {'action': action})
 
 
 class TrajectoriesBuffer(object):
@@ -17,7 +40,7 @@ class TrajectoriesBuffer(object):
         self.trajectories = []
 
     def __len__(self):
-        len(self.trajectories)
+        return len(self.trajectories)
 
     def add(self, trajectories):
         if isinstance(trajectories, list):
