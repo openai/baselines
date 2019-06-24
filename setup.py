2bca7901f51c88cdef3ca0666c6a87c454a4dbe8
Marcin Michalski <cyfra@users.noreply.github.com>
2019-06-24 19:19:01 +0200

    Updating the version to 0.1.6 (#933)
    Updating the version in setup.py to avoid conflict with the old (>1 year old) version in pypi.

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(name='baselines',
       author='OpenAI',
       url='https://github.com/openai/baselines',
       author_email='gym@openai.com',
-      version='0.1.5')
+      version='0.1.6')
 
 
 # ensure there is some tensorflow build with version above 1.4
