Bug fix in TRPO (logger with zero gradient) (#496)
* Update trpo_mpi.py

The bug is still there. I don't know guys how it works for you, but in my case if I get zero gradient then `mean_losses` are undefined and cannot be zipped in a logger for loop. So it always throws Exception and closes the program.

* Update changelog.rst

* Update changelog.rst

* Update changelog.rst

stable_baselines/trpo_mpi/trpo_mpi.py
@@ -413,6 +413,9 @@ class TRPO(ActorCriticRLModel):
                                 # list of tuples
                                 paramsums = MPI.COMM_WORLD.allgather((thnew.sum(), self.vfadam.getflat().sum()))
                                 assert all(np.allclose(ps, paramsums[0]) for ps in paramsums[1:])
+                            
+                            for (loss_name, loss_val) in zip(self.loss_names, mean_losses):
+                                logger.record_tabular(loss_name, loss_val)
 
                         with self.timed("vf"):
                             for _ in range(self.vf_iters):
@@ -423,9 +426,6 @@ class TRPO(ActorCriticRLModel):
                                                                          shuffle=True):
                                     grad = self.allmean(self.compute_vflossandgrad(mbob, mbob, mbret, sess=self.sess))
                                     self.vfadam.update(grad, self.vf_stepsize)
-
-                    for (loss_name, loss_val) in zip(self.loss_names, mean_losses):
-                        logger.record_tabular(loss_name, loss_val)
 
                     logger.record_tabular("explained_variance_tdlam_before",
                                           explained_variance(vpredbefore, tdlamret))