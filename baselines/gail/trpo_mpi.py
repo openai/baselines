703b87196b894b8bb9a92250f0799a75b908acab
Richard Wu <richardwu1997@gmail.com>
2020-03-05 17:52:24 -0500

    Fix advantage standardization when std is 0.

--- baselines/gail/trpo_mpi.py
+++ baselines/gail/trpo_mpi.py
@@ -249,7 +249,7 @@ def learn(env, policy_func, reward_giver, expert_dataset, rank,
             # ob, ac, atarg, ret, td1ret = map(np.concatenate, (obs, acs, atargs, rets, td1rets))
             ob, ac, atarg, tdlamret = seg["ob"], seg["ac"], seg["adv"], seg["tdlamret"]
             vpredbefore = seg["vpred"]  # predicted value function before udpate
-            atarg = (atarg - atarg.mean()) / atarg.std()  # standardized advantage function estimate
+            atarg = (atarg - atarg.mean()) / (atarg.std() or 1.)  # standardized advantage function estimate
 
             if hasattr(pi, "ob_rms"): pi.ob_rms.update(ob)  # update running mean/std for policy
 
