782bbdbaedd0bfd127f5f70577bf4376e83a927f
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-01-24 01:14:18 -0500

    spelling: macos
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- baselines/common/mpi_util.py
+++ baselines/common/mpi_util.py
@@ -39,7 +39,7 @@ def setup_mpi_gpus():
     Set CUDA_VISIBLE_DEVICES to MPI rank if not already set
     """
     if 'CUDA_VISIBLE_DEVICES' not in os.environ:
-        if sys.platform == 'darwin': # This Assumes if you're on OSX you're just
+        if sys.platform == 'darwin': # This Assumes if you're on macOS you're just
             ids = []                 # doing a smoke test and don't want GPUs
         else:
             lrank, _lsize = get_local_rank_size(MPI.COMM_WORLD)
