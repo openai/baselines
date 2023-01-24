b8be389e4558ded63f714be88889a96d8620903d
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-01-24 00:56:37 -0500

    spelling: intended
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- baselines/common/misc_util.py
+++ baselines/common/misc_util.py
@@ -192,7 +192,7 @@ def relatively_safe_pickle_dump(obj, path, compression=False):
         - it is sometimes possible that we end up with useless temp file which needs to be
           deleted manually (it will be removed automatically on the next function call)
 
-    The indended use case is periodic checkpoints of experiment state, such that we never
+    The intended use case is periodic checkpoints of experiment state, such that we never
     corrupt previous checkpoints if the current one fails.
 
     Parameters
